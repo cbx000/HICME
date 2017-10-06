e42cff53341b740a7866b321653dba179f9795b3
Ai Xin <scicomath@gmail.com>
2017-10-06 22:51:33 +0800

    更新HIeB类的定义

--- HIeB.hpp
+++ HIeB.hpp
@@ -2,7 +2,6 @@
 
 #define HIEBHPP
 
-#include <cmath>
 #include <string>
 
 class HIeB
@@ -10,10 +9,12 @@ class HIeB
 public:
   HIeB();
   double eBx, eBy, eBz; // 重离子碰撞中产生的磁场
-  double x, y, z, t; // 计算磁场的时空坐标
-  double b; // 碰撞参量
-  double sigma, sigmaChi; // 电导率和手征磁导率(目前用不到)
-
+  
+  // 设置场点的时空坐标
+  void SetSpaceTime(double x, double y, double z, double t);
+  // 设置场点的时空坐标(以另外一种坐标形式)
+  void SetSpaceTime_tau(double x, double y, double tau, double eta);
+  
   // 设置与获取质心系能量
   void SetSqrtS(double sqrtS);
   double GetSqrtS() const;
@@ -23,14 +24,23 @@ public:
   // 设置与获取初始快度
   void SetY0(double Y0);
   double GetY0() const;
+
+  // 设置与获取碰撞参数
+  void SetB(double b);
+  double GetB() const;
+  
   // 设置与获取核类型
   void SetNucleiType(std::string nuclei); // method to set nuclei parameters
   std::string GetNucleiType() const; // method to get nuclei name
 
-  void CalVaccumEB(int comp); // 计算真空磁场
-  void CalQGPEB(int comp); // 计算QGP中的磁场
+  void CalVaccumEB(); // 计算真空磁场
+  void CalQGPEB(); // 计算QGP中的磁场
   
 private:
+  double mx, my, mz, mt; // 计算磁场的时空坐标
+  double meta, mtau; // 用eta和tau来表示z和t
+
+  double mb; // 碰撞参量
   double mGamma; // 洛伦兹收缩因子gamma
   double mSqrtS; // 质心系能量
   double mY0; // 初始快度
@@ -44,15 +54,8 @@ private:
   
   double ma; // 碰撞后快度分布参数
 
+  double rhoFun_Ai(); // 核密度分布函数
 
-  char mRegionType, mFlag;
-
-  double rhoFun_Ai();
-
-  int eB_Part_Int(const int *ndim, const double xx[], const int *ncomp, double ff[], void *userdata);
-  int eB_Spec_Int(const int *ndim, const double xx[], const int *ncomp, double ff[], void *userdata);
-  int eB_Intgrand(const int *ndim, const double xx[], const int *ncomp, double ff[], void *userdata);
-  
 };
 
 
