74b5116f0d68e11a540ed840ab78d0fef88e65aa
Ai Xin <scicomath@gmail.com>
2017-10-07 11:23:25 +0800

    添加平方函数

---
+++ sq.c
@@ -0,0 +1,4 @@
+#include "sq.h"
+
+extern double Sq(double x);
+extern double Pow3(double x);
---
+++ sq.h
@@ -0,0 +1,12 @@
+#ifndef SQ_H
+#define SQ_H
+
+inline double Sq(double x) {
+  return x*x;
+}
+
+inline double Pow3(double x) {
+  return x*x*x;
+}
+
+#endif
