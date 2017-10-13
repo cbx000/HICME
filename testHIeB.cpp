e80313d94bd294b81de655434f5c51f2562c80cb
Ai Xin <scicomath@gmail.com>
2017-10-13 11:36:09 +0800

    完善测试代码

--- testHIeB.cpp
+++ testHIeB.cpp
@@ -2,69 +2,91 @@
 #include <iomanip>
 #include <cmath>
 #include "HIeB.hpp"
+using namespace std;
 
 int main(int argc, char* argv[])
 {
-  double sqrtS, gamma, Y0, tolerance = 1e-3;
+  double sqrtS, gamma, Y0, tolerance = 1e-3, t;
   HIeB myeB;
 
-  std::setprecision(9);
+  setprecision(9);
   
   // test SetSqrtS()
-  std::cout << "Testing SetSqrtS() ... ";
+  cout << "Testing SetSqrtS() ... ";
   myeB.SetSqrtS(200.0);
   gamma = myeB.GetGamma();
   Y0 = myeB.GetY0();
   if (fabs(gamma - 106.579) < tolerance && fabs(Y0 - 5.36201) < tolerance) {
-    std::cout << "OK\n";
+    cout << "OK\n";
   } else {
-    std::cout << "fail!" << " |gamma - 106.579| = " << fabs(gamma - 106.579)
-	      << " |Y0 - 5.36201| = " << fabs(Y0 - 5.36201) << std::endl;
+    cout << "fail!" << " |gamma - 106.579| = " << fabs(gamma - 106.579)
+	      << " |Y0 - 5.36201| = " << fabs(Y0 - 5.36201) << endl;
   }
   
   // test SetGamma()
-  std::cout << "Testing SetGamma() ... ";
+  cout << "Testing SetGamma() ... ";
   myeB.SetGamma(34.1052);
   sqrtS = myeB.GetSqrtS();
   Y0 = myeB.GetY0();
   if (fabs(sqrtS - 64.0) < tolerance && fabs(Y0 - 4.22238) < tolerance) {
-    std::cout << "OK\n";
+    cout << "OK\n";
   } else {
-    std::cout << "fail!\n";
+    cout << "fail!\n";
   }
 
   // test SetY0
-  std::cout << "Testing SetY0() ... ";
+  cout << "Testing SetY0() ... ";
   myeB.SetY0(4.6688);
   sqrtS = myeB.GetSqrtS();
   gamma = myeB.GetGamma();
   if (fabs(sqrtS - 100.0) < tolerance && fabs(gamma - 53.2896) < tolerance) {
-    std::cout << "OK\n";
+    cout << "OK\n";
   } else {
-    std::cout << "fail!\n";
-    std::cout << "    sqrtS - 100.0 = " << sqrtS - 100.0
-	      << "    Y0 - 4.6688 = " << gamma - 53.2896 << std::endl;
+    cout << "fail!\n";
+    cout << "    sqrtS - 100.0 = " << sqrtS - 100.0
+	      << "    Y0 - 4.6688 = " << gamma - 53.2896 << endl;
   }
-  myeB.SetSpaceTime(0.0, 0.0, 0.0, 0.1);
-  myeB.SetSqrtS(200.0);
-  
-  myeB.CalVaccumEB();
-  std::cout << "x = " << myeB.GetX() << " "
-  	    << "y = " << myeB.GetY() << " "
-  	    << "z = " << myeB.GetZ() << " "
-  	    << "t = " << myeB.GetT() << std::endl
-  	    << "eBy = " << myeB.eBy << std::endl;
-  
 
-  // 测试QGP响应
-  myeB.SetSpaceTime(0.0, 0.0, 0.0, 0.2);
-  myeB.CalOriginQGPeB();
-  std::cout << "x = " << myeB.GetX() << " "
-  	    << "y = " << myeB.GetY() << " "
-  	    << "z = " << myeB.GetZ() << " "
-  	    << "t = " << myeB.GetT() << std::endl
-        << "eBy = " << myeB.eBy << std::endl;
-        
+  // 测试真空中磁场
+  cout << "测试原点真空磁场: " << endl;
+  double preCaleB[] = {68326.4, 215.59, 43.2741, 17.5784, 9.21409, 5.47008, 3.6497, 2.49731, 1.88101, 1.4337};
+  
+  myeB.SetSqrtS(200.0);
+  for (int i = 0; i < 10; i++) {
+    t = i*5.0/10.0;
+    myeB.SetSpaceTime(0.0, 0.0, 0.0, t);
+    myeB.CalVaccumEB();
+    cout << "t = " << myeB.GetT() << " eBy = " << myeB.eBy << " ... ";
+    if (fabs(myeB.eBy - preCaleB[i])/preCaleB[i] < 0.01) {
+      cout << "OK";
+    } else {
+      cout << "fail";
+    }
+    cout << endl;
+  }
+
+  // 测试原点QGP响应
+  cout << "测试原点QGP响应磁场" << endl;
+  double preCalQGPeB[] = {68326.4, 2714.67, 1338.61, 871.988, 633.137, 485.838, 384.763, 310.534, 253.488, 208.269};
+
+  for (int i = 0; i < 10; i++) {
+    t = i*5.0/10.0;
+    myeB.SetSpaceTime(0.0, 0.0, 0.0, t);
+    if (t <= myeB.GetTau0() ) {
+      myeB.CalVaccumEB();
+    } else {
+      myeB.CalOriginQGPeB();
+    }
+    cout << "t = " << myeB.GetT() << " eBy = " << myeB.eBy << " ... ";
+    if (fabs(myeB.eBy - preCalQGPeB[i])/preCalQGPeB[i] < 0.01) {
+      cout << "OK";
+    } else {
+      cout << "fail";
+    }
+    cout << endl;
+    
+  }
+  
   myeB.SetSpaceTime_tau(0.25, 0.1);
   myeB.CalQGPeB();
   std::cout << "x = " << myeB.GetX() << " "
